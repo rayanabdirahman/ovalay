53b9e3ca5a3f05ec2fc8857d5b38795933367195
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-07-13 17:01:18 +0100

    move root navigator to seprate file

---
+++ app/navigation/RootNavigator.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { RootStackParamList } from '../types'
+import BottomTabNavigator from './BottomTabNavigator'
+import NotFoundScreen from '../screens/NotFoundScreen'
+
+// Root stack navigatior
+// screens will display when user has signed in
+const Stack = createStackNavigator<RootStackParamList>()
+export default function RootNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Root" component={BottomTabNavigator} />
+      <Stack.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: 'Oops!' }}
+      />
+    </Stack.Navigator>
+  )
+}
--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -1,34 +1,22 @@
 import React from 'react'
 import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native'
 import { ColorSchemeName } from 'react-native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { RootStackParamList } from '../types';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import AuthenticationNavigator from './AuthenticationNavigator';
-import BottomTabNavigator from './BottomTabNavigator';
+import AuthenticationNavigator from './AuthenticationNavigator'
+import RootNavigator from './RootNavigator'
 
 type NavigationContainerParamsList = {
-  colorScheme?: ColorSchemeName,
+  colorScheme?: ColorSchemeName
   isUserAuthenticated: boolean
 }
 
 // Navigation container to determin which navigation screens to show
-export default function Navigation({ colorScheme, isUserAuthenticated }: NavigationContainerParamsList) {
+export default function Navigation({
+  colorScheme,
+  isUserAuthenticated
+}: NavigationContainerParamsList) {
   return (
     <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      { isUserAuthenticated ? <RootNavigator /> : <AuthenticationNavigator /> }
+      {isUserAuthenticated ? <RootNavigator /> : <AuthenticationNavigator />}
     </NavigationContainer>
   )
-}
-
-// Root stack navigatior
-// screens will display when user has signed in
-const Stack = createStackNavigator<RootStackParamList>();
-function RootNavigator() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-    </Stack.Navigator>
-  );
 }
