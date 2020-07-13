b48fed31d86b8a4fcae09118b72cc0fcb7c42ab0
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-07-13 17:31:08 +0100

    add icons to bottom nav

--- app/navigation/BottomTabNavigator.tsx
+++ app/navigation/BottomTabNavigator.tsx
@@ -1,19 +1,51 @@
 import React from 'react'
+import { Image, ImageProps } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { BottomTabParamList } from '../types'
 import { RootScreenName } from '../constants/ScreenNames'
 import { ExploreScreen, SearchScreen, WishListScreen, ProfileScreen } from '../screens/Root'
+
+function TabBarIcon({ image, color }: { image: ImageProps; color: string }) {
+  return <Image source={image} style={{ height: 24, width: 24, tintColor: color }} resizeMode="contain"/>
+}
 
 // Bottom tab navigatior
 // These screens will display when user has signed in
 const BottomTab = createBottomTabNavigator<BottomTabParamList>()
 export default function BottomTabNavigator() {
   return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen name={RootScreenName.EXPLORE} component={ExploreScreen}/>
-      <BottomTab.Screen name={RootScreenName.SEARCH} component={SearchScreen}/>
-      <BottomTab.Screen name={RootScreenName.WISHLIST} component={WishListScreen}/>
-      <BottomTab.Screen name={RootScreenName.PROFILE} component={ProfileScreen}/>
+    <BottomTab.Navigator tabBarOptions={{ activeTintColor: '#0046FF', inactiveTintColor: '#BFC5D2', style: { backgroundColor: '#fff' }}}>
+      <BottomTab.Screen
+        name={RootScreenName.EXPLORE}
+        component={ExploreScreen}
+        options={{ 
+          tabBarIcon: ({ color }) => <TabBarIcon image={require('../assets/icons/basket.png')} color={color} /> 
+        }} 
+      />
+
+      <BottomTab.Screen
+        name={RootScreenName.SEARCH}
+        component={SearchScreen}
+        options={{ 
+          tabBarIcon: ({ color }) => <TabBarIcon image={require('../assets/icons/search.png')} color={color} />
+        }}
+      />
+
+      <BottomTab.Screen
+        name={RootScreenName.WISHLIST}
+        component={WishListScreen}
+        options={{ 
+          tabBarIcon: ({ color }) => <TabBarIcon image={require('../assets/icons/heart.png')} color={color} />
+        }}
+      />
+
+      <BottomTab.Screen
+        name={RootScreenName.PROFILE}
+        component={ProfileScreen}
+        options={{ 
+          tabBarIcon: ({ color }) => <TabBarIcon image={require('../assets/icons/person.png')} color={color} />
+        }}
+      />
     </BottomTab.Navigator>
   )
 }
