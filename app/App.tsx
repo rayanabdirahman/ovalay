5b7e746c852043bd8a6efb0db730406ccbfa6923
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-07-14 23:57:43 +0100

    Ensure theme props are being passed down to App component

--- app/App.tsx
+++ app/App.tsx
@@ -7,28 +7,22 @@ import Navigation from './navigation'
 import { theme } from './components/Themed'
 import { store } from './store'
 
+// ThemeProvider component provideds access to styled component theme
 function App() {
-  const isLoadingComplete = true
   const isUserAuthenticated = false
-
-  if (!isLoadingComplete) {
-    return null
-  } else {
-    return (
+  return (
+    <ThemeProvider theme={theme}>
       <SafeAreaProvider>
         <Navigation isUserAuthenticated={isUserAuthenticated}  />
         <StatusBar />
       </SafeAreaProvider>
-    )
-  }
+    </ThemeProvider>
+  )
 }
 
 // Provider component provides access to application state
-// ThemeProvider component provideds access to styled component themes
 export default () => (
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </Provider>
 )
