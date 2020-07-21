73fb512e904f31b5fe618c06a7bef9067c2627b2
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-07-21 16:33:48 +0100

    add hero boilerplate

---
+++ landing/components/Hero.tsx
@@ -0,0 +1,12 @@
+export const Hero: React.FunctionComponent = () => (
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div className="col-10 text-center">
+          <h1 className="display-4">Redefining the shopping experience</h1>
+          <p className="lead">Shop your favourite high street stores and brands from one place </p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
