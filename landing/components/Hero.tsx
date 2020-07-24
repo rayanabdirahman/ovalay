74cbd97c6d63442de6ac260c2f8970fad29c5070
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-07-24 19:12:13 +0100

    add hero update

---
+++ landing/components/Hero.tsx
@@ -0,0 +1,6 @@
+export const Hero: React.FunctionComponent = () => (
+  <section className="jumbotron text-center">
+    <h1 className="display-4">Shopping, but better</h1>
+    <p className="lead">We’re redefining the shopping experience</p>
+  </section>
+)
--- landing/pages/index.tsx
+++ landing/pages/index.tsx
@@ -1,7 +1,9 @@
 import Layout from '../components/Layout'
+import { Hero } from '../components/Hero'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+    <Hero />
   </Layout>
 )
 
