c365d05a6098954165b86eab3586de7a0de57b9f
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-07-24 21:45:37 +0100

    add selling points

---
+++ landing/components/SellingPoints.tsx
@@ -0,0 +1,28 @@
+export const SellingPoints: React.FunctionComponent = () => (
+  <section>
+    <div className="row align-items-center justify-content-start mb-5">
+      <div className="col-5">
+        <h3>Shop your favourite high street stores and brands from one place</h3>
+      </div>
+      <div className="col-6">
+        <img src="/shoes.png" alt="" width="100%"/>
+      </div>
+    </div>
+    <div className="row align-items-center justify-content-start flex-row-reverse mb-5">
+      <div className="col-5">
+        <h3>Follow the stores you love for better recommendations</h3>
+      </div>
+      <div className="col-6">
+        <img src="/recommendations.png" alt="" width="100%"/>
+      </div>
+    </div>
+    <div className="row align-items-center justify-content-start mb-5">
+      <div className="col-5">
+        <h3>Keep up to date with the latest sales and drops</h3>
+      </div>
+      <div className="col-6">
+        <img src="/sales.png" alt="" width="100%"/>
+      </div>
+    </div>
+  </section>
+)
--- landing/pages/index.tsx
+++ landing/pages/index.tsx
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout'
 import { Hero } from '../components/Hero'
+import { SellingPoints } from '../components/SellingPoints'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <Hero />
+    <SellingPoints />
   </Layout>
 )
 
