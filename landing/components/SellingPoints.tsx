6f2f355d91c20b6e55b297d37f538629c127ea74
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-07-25 09:07:26 +0100

    change h3 tag to h2

--- landing/components/SellingPoints.tsx
+++ landing/components/SellingPoints.tsx
@@ -2,7 +2,7 @@ export const SellingPoints: React.FunctionComponent = () => (
   <section>
     <div className="row align-items-center justify-content-start mb-5">
       <div className="col-5">
-        <h3>Shop your favourite high street stores and brands from one place</h3>
+        <h2>Shop your favourite high street stores and brands from one place</h2>
       </div>
       <div className="col-6">
         <img src="/shoes.png" alt="" width="100%"/>
@@ -10,7 +10,7 @@ export const SellingPoints: React.FunctionComponent = () => (
     </div>
     <div className="row align-items-center justify-content-start flex-row-reverse mb-5">
       <div className="col-5">
-        <h3>Follow the stores you love for better recommendations</h3>
+        <h2>Follow the stores you love for better recommendations</h2>
       </div>
       <div className="col-6">
         <img src="/recommendations.png" alt="" width="100%"/>
@@ -18,7 +18,7 @@ export const SellingPoints: React.FunctionComponent = () => (
     </div>
     <div className="row align-items-center justify-content-start mb-5">
       <div className="col-5">
-        <h3>Keep up to date with the latest sales and drops</h3>
+        <h2>Keep up to date with the latest sales and drops</h2>
       </div>
       <div className="col-6">
         <img src="/sales.png" alt="" width="100%"/>
