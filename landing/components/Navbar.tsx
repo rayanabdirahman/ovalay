b456f0ccff68f1207a88774ac40bd23d37f31cdc
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-02 18:52:31 +0100

    add container to navbar

--- landing/components/Navbar.tsx
+++ landing/components/Navbar.tsx
@@ -2,25 +2,27 @@ import Link from 'next/link'
 
 export const Navbar: React.FunctionComponent = () => (
   <div className="navbar navbar-expand-lg">
-    <a className="navbar-brand" href="#">mainstreet</a>
-    {/* <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link  href="#">
-            <a className="nav-link">Store owners</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link  href="#">
-            <a className="nav-link">Contact us</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link  href="#">
-            <a className="nav-link">Join waitlist</a>
-          </Link>
-        </li>
-      </ul>
-    </div> */}
+    <div className="container">
+      <a className="navbar-brand" href="#">mainstreet</a>
+      {/* <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link  href="#">
+              <a className="nav-link">Store owners</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link  href="#">
+              <a className="nav-link">Contact us</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link  href="#">
+              <a className="nav-link">Join waitlist</a>
+            </Link>
+          </li>
+        </ul>
+      </div> */}
+    </div>
   </div>
 )
