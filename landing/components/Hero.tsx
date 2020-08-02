d84b190e560de779f9c17dff3f490095fdf1bd1f
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-02 17:18:54 +0100

    add new styles for hero and mailchimp form

--- landing/components/Hero.tsx
+++ landing/components/Hero.tsx
@@ -6,7 +6,8 @@ export const Hero: React.FunctionComponent = () => (
       <div className="row">
         <div className="col-6">
           <h1 className="display-4">The online mall for trainers</h1>
-          <p className="lead">Join our waitlist to get early access</p>
+          <p className="lead mb-3">Join our waitlist to get early access</p>
+          <MailChimpForm />
         </div>
         <div className="col-6">
           <img src="/recommendations.png" alt="" width="100%"/>
