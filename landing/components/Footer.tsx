4bff195c1ab55fd2ac06f1ddcc87cfe6a9fa67d1
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-02 19:24:47 +0100

    update footer component

--- landing/components/Footer.tsx
+++ landing/components/Footer.tsx
@@ -1,36 +1,54 @@
 import Link from 'next/link'
+import { MailChimpForm } from './MailChimpForm'
 
 export const Footer: React.FunctionComponent = () => (
   <footer className="footer">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-6">
-          <p>made with ♥️  in London</p>
+    <div className="jumbotron">
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-3 text-center">
+            <h2 className="h1 mb-5">We’re redefining the shopping experience</h2>
+          </div>
         </div>
-        <div className="col-6">
-          <ul className="social-links">
-            <li className="social-item">              
-              <Link href="#">
-                <a>
-                  <img src="/twitter-white.png" alt="twitter"/>
-                </a>
-              </Link>
-            </li>
-            <li className="social-item">              
-              <Link href="#">
-                <a>
-                  <img src="/instagram-white.png" alt="instagram"/>
-                </a>
-              </Link>
-            </li>
-            <li className="social-item">              
-              <Link href="#">
-                <a>
-                  <img src="/mail-white.png" alt="mail"/>
-                </a>
-              </Link>
-            </li>
-          </ul>
+        <div className="row justify-content-center">
+          <div className="col-6 justify-content-center">
+            <div className="col-6 m-auto">
+              <p className="lead mb-3">Join our waitlist to get early access</p>
+              <MailChimpForm />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom container">
+        <div className="row">
+          <div className="col-6">
+            <p>made with ♥️  in London</p>
+          </div>
+          <div className="col-6">
+            <ul className="social-links">
+              <li className="social-item">              
+                <Link href="#">
+                  <a>
+                    <img src="/twitter-white.png" alt="twitter"/>
+                  </a>
+                </Link>
+              </li>
+              <li className="social-item">              
+                <Link href="#">
+                  <a>
+                    <img src="/instagram-white.png" alt="instagram"/>
+                  </a>
+                </Link>
+              </li>
+              <li className="social-item">              
+                <Link href="#">
+                  <a>
+                    <img src="/mail-white.png" alt="mail"/>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
