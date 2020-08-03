d6effeac0aede8fb5d8e351c9cb33503bd714a80
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-03 23:53:22 +0100

    add styles for product revview feed card

--- app/components/FeedCard.tsx
+++ app/components/FeedCard.tsx
@@ -34,7 +34,7 @@ const Head = styled.View`
 const Body = styled.View`
   display: flex;
   flex-direction: row;
-  margin-left: 32px;
+  margin-left: 24px;
 `
 
 const Image = styled.Image<ImageProps>`
@@ -42,7 +42,12 @@ const Image = styled.Image<ImageProps>`
   height: ${ props => props.small ? '64px' : '140px' };
   margin-right: ${ props => props.small ? '8px' : '16px' }; 
   border-radius: 8px;
-  /* margin-bottom: 0; */
+`
+
+const StarImage = styled.Image<ImageProps>`
+  width: 16px;
+  height: 16px;
+  margin-right: ${ props => props.small ? '8px' : '16px' }; 
 `
 
 export const FeedCard = ({
@@ -116,18 +121,27 @@ export const FeedCard = ({
             <SmallText light>2 hour</SmallText>
           </DefaultView>
         </Head>
-        <Body>
-          {
-            [
-              require('../assets/images/nike-95.jpg'),
-              require('../assets/images/yeezy.jpg'),
-            ].map((image, index, arr) => {
-              return (arr.length > 2) ? 
-                <Image key={`product-image--${index}`} small source={image} resizeMode="cover" /> : 
-                <Image key={`product-image--${index}`} source={image} resizeMode="cover" />
-            })
-          }
-          { imagesArr.length > 2 ? <Button secondary title={`+ ${images.length - 3}`} onPress={() => alert('clciked')} /> : null }
+        <Body style={{ flexDirection: "column" }}>
+          <DefaultView style={{ flexDirection: "row", marginBottom: 16 }}>
+            {
+              [
+                require('../assets/icons/star-filled.png'),
+                require('../assets/icons/star-filled.png'),
+                require('../assets/icons/star-filled.png'),
+                require('../assets/icons/star-filled.png'),
+                require('../assets/icons/star.png'),
+              ].map((image, index ) => {
+                  return <StarImage key={`review-star--${index}`} small source={image} resizeMode="contain" /> 
+              })
+            }
+          </DefaultView>
+
+          <DefaultView style={{ flexDirection: "row", justifyContent: "space-between" }}>
+            <DefaultView style={{ maxWidth: 250 }}>
+              <Text>Extremely comfortable couldn't recommend them enough</Text>
+            </DefaultView>
+            <Image small source={require('../assets/images/fila.jpg')} resizeMode="cover" />
+          </DefaultView>
         </Body>
       </Card>
       
