18da90db82fff025648079371b69037c02994edf
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-04 11:00:06 +0100

    add shared content container component

--- app/screens/Root/FeedScreen.tsx
+++ app/screens/Root/FeedScreen.tsx
@@ -1,30 +1,20 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import * as React from 'react'
 
-
 import { BottomTabParamList } from '../../navigation/types'
 import { RootScreenName } from '../../navigation/ScreenNames'
-import { Layout, Text, FeedCard } from '../../components'
-import styled from 'styled-components/native'
+import { Layout, ContentContainer, Text, FeedCard } from '../../components'
 import { ScrollView } from 'react-native'
 
-const Container = styled.View`
-  flex: 1;
-  padding: 32px 24px;
-  margin-top: 32px;
-`
-
-export const FeedScreen = ({
-  navigation,
-}: StackScreenProps<BottomTabParamList, RootScreenName.FEED>) => {
+export const FeedScreen = ({ navigation }: StackScreenProps<BottomTabParamList, RootScreenName.FEED>) => {
   return (
     <Layout fullwidth>
-      <Container>
+      <ContentContainer>
         <Text h1>Feed</Text>
         <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
           <FeedCard />
         </ScrollView>
-      </Container>
+      </ContentContainer>
     </Layout>
   )
 }
