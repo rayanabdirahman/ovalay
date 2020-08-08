7eb280d767898cb5fc60f323b8db23c15bc3b698
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-08 12:23:44 +0100

    add inversify config for category controller and service

--- services/product/src/inversify.config.ts
+++ services/product/src/inversify.config.ts
@@ -7,14 +7,18 @@ import { ProductService, ProductServiceImpl } from './service/product.service'
 import { ProductRepository, ProductRepositoryImpl } from './data_access/repository/product.repository'
 import { CategoryRepository, CategoryRepositoryImpl } from './data_access/repository/category.repository'
 import { OwnerRepository, OwnerRepositoryImpl } from './data_access/repository/owner.repository'
+import CategoryController from './api/category/category.controller'
+import { CategoryService, CategoryServiceImpl } from './service/category.service'
 
 const container = new Container()
 
 // controllers
 container.bind<RegistrableController>(TYPES.Controller).to(ProductController)
+container.bind<RegistrableController>(TYPES.Controller).to(CategoryController)
 
 // services
 container.bind<ProductService>(TYPES.ProductService).to(ProductServiceImpl)
+container.bind<CategoryService>(TYPES.CategoryService).to(CategoryServiceImpl)
 
 // repository
 container.bind<ProductRepository>(TYPES.ProductRepository).to(ProductRepositoryImpl)
