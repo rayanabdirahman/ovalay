62e096bb2e9ef7c0c5d19c4b75342a6a37bc4b3c
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-09 13:20:40 +0100

    set env variables for test setup

--- services/product/src/test/setup.ts
+++ services/product/src/test/setup.ts
@@ -7,6 +7,9 @@ let mongo: MongoMemoryServer
 beforeAll(async () => {
   // set env variables
   process.env.JWT_SECRET = 'randomstringforJWT'
+  process.env.AWS_S3_PRODUCT_PHOTOS_BUCKET = 'awsS3ProductBucket'
+  process.env.AWS_ACCESS_KEY_ID = 'awsS3AccessKey'
+  process.env.AWS_SECRET_ACCESS_KEY = 'awsS3SecretAccessKey'
 
   // create instance of inmemory mongo database
   mongo = new MongoMemoryServer()
