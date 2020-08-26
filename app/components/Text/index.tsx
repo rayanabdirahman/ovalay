20772d201ae9f3631ce375f344dfdb1e9875b569
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-26 23:09:35 +0100

    update Text component to include light and bold text

--- app/components/Text/index.tsx
+++ app/components/Text/index.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components/native'
 import { Text as DefaultText } from 'react-native'
 
-type Props = DefaultText['props']
+type Props = DefaultText['props'] & {
+  light?: boolean
+}
 
 const BaseText = styled.Text<Props>`
   font-size: 16px;
@@ -13,4 +15,10 @@ export const ScreenTitle = styled(BaseText)`
   font-size: 24px;
   margin-bottom: 32px;
   font-weight: 700;
+`
+export const Text = styled(BaseText)`
+  font-size: 16px;
+  margin-bottom: 4px;
+  font-weight: ${ ({ bold }) => bold ? 700 : 400 };
+  color: ${ ({ light, theme }) => light ? theme.colour.lightgrey : theme.colour.black };
 `
