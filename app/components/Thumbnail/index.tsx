0d80b728ab42a5d19a6e6cfce36d9c14c8093346
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-30 13:23:13 +0100

    add small margin bottom to thumbnail

--- app/components/Thumbnail/index.tsx
+++ app/components/Thumbnail/index.tsx
@@ -6,12 +6,13 @@ import { Text } from '../../components/Text'
 export type Props = DefaultThumbnail['props'] & {
   name?: string
   subtitle?: string
-  center?: boolean
+  center?: boolean,
+  smallMarginBottom?: boolean
 }
 
 export const Thumbnail = (props: Props) => (
   <TouchableOpacity
-    style={{ flexDirection: "row", marginBottom: 16 }}
+    style={{ flexDirection: "row", marginBottom: props.smallMarginBottom ? 4 : 16 }}
     onPress={() => alert('profile')}>
     <DefaultThumbnail {...props} />
     <DefaultView style={{ flexDirection: "column", marginLeft: 8 }}>
