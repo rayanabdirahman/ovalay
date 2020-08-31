bec6092b40eb97c84ab66030003cd77a539513e1
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-08-31 18:45:35 +0100

    add picker and button to Product screen

--- app/screens/app/Product.tsx
+++ app/screens/app/Product.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { View as DefaultView } from 'react-native'
+import React, { useState } from 'react'
+import { View as DefaultView, ScrollView } from 'react-native'
+import { Picker, Icon } from "native-base"
 import { orders } from '../../temp/data'
-import { Text } from '../../components/Text'
 import { Layout } from '../../components/Layout'
-import { Thumbnail } from '../../components/Thumbnail'
-import { Image } from '../../components/Image'
 import { ProductProfile } from '../../components/blocks/ProductProfile'
+import { Button } from '../../components/Button'
+import { theme } from '../../components/Theme'
 
 export default function Product({ route }: any) {
+  const [selectedSize, setSelectedSize] = useState("7");
   const { _id } = route.params
   const product = orders.map(order => order.product.find(product => product._id === _id))[0]
   return (
-    <Layout fullwidth withNoContainer>
-      {/* { console.log('product', product ) } */}
+  <Layout fullwidth withNoContainer>
+    <ScrollView>
       <ProductProfile product={product} />
-    </Layout>
+    </ScrollView>
+    <DefaultView style={{ flexDirection: "row", alignItems: "center", marginRight: 8, marginLeft: 8, marginBottom: 32 }}>
+      <DefaultView style={{ flex: 0.3 }}>
+        <Picker
+          mode="dropdown"
+          iosHeader="Select a Size"
+          iosIcon={<Icon name="arrow-down" />}
+          style={{ borderColor: theme.colour.lightgrey, borderWidth: 1, height: 60, borderStyle: "solid" }}
+          selectedValue={selectedSize}
+          onValueChange={(itemValue, itemIndex) => setSelectedSize(itemValue)}>
+          <Picker.Item label="7" value="7" />
+          <Picker.Item label="8" value="8" />
+        </Picker>
+      </DefaultView>
+        
+      <DefaultView style={{ flex: 0.7 }} >
+      <Button primary title="Add to cart" onPress={() => alert("add to cart")} />
+      </DefaultView>
+    </DefaultView>
+  </Layout>
   )
 }
