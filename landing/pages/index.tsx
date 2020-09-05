e68cc63585f27ac71592ee507258b1b09024323e
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-09-05 13:21:31 +0100

    add meta tags

--- landing/pages/index.tsx
+++ landing/pages/index.tsx
@@ -1,13 +1,10 @@
-import Head from 'next/head'
+import Head from '../components/Head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head/>
     </div>
   )
 }
