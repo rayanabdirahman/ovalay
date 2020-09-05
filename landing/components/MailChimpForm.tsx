22f5a93664d526b151de6d5a321f8bb64d958e61
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-09-05 11:58:03 +0100

    remove old landing folder

--- landing/components/MailChimpForm.tsx
+++
@@ -1,8 +0,0 @@
-export const MailChimpForm: React.FunctionComponent = () => (
-  <form className="form" action="">
-    <div className="form-group">
-      <input placeholder="email address" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    </div>
-    <button type="submit">Join waitlist</button>
-  </form>
-)
