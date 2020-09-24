28f48f0bfe9a691f047dbe0f1a1db1c90c893d06
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-09-24 21:48:17 +0100

    load custom fonts

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text } from 'react-native'
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_900Black } from '@expo-google-fonts/inter'
 import Navigation from './navigation'
 import { store, State } from './store'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
@@ -15,8 +16,16 @@ function App() {
   )
 }
 
-export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+export default () => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_900Black,
+  });
+
+  return(
+    <Provider store={store}>
+      { fontsLoaded ? <App /> : <Text>App loading</Text> }
+    </Provider>
+  )
+}
