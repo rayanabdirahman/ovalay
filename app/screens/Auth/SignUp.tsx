190d8604079cd3c834e15e2f443a362ff00e0c93
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-09-25 15:52:47 +0100

    add input styles for sign up screen

--- app/screens/Auth/SignUp.tsx
+++ app/screens/Auth/SignUp.tsx
@@ -2,20 +2,24 @@ import React from 'react'
 import { StackScreenProps } from '@react-navigation/stack'
 import { View as DefaultView } from 'react-native'
 import { AuthScreenName, AuthStackParamList } from '../../navigation/types'
-import { Layout, Text, Button, Link } from '../../components'
+import { Layout, Text, Button, Link, Input } from '../../components'
 
 const SignUp = ({ navigation }: StackScreenProps<AuthStackParamList, AuthScreenName.SIGN_UP>) => (
   <Layout title="Sign up">
     <DefaultView style={{ marginBottom: 24 }}>
-      <Text>Create an account so you can shop your favourite stores from one place</Text>
+      <Text style={{ marginBottom: 24 }}>
+        Create an account so you can shop your favourite stores from one place
+      </Text>
+      <Input placeholder="Username" />
+      <Input placeholder="Full name" />
+      <Input placeholder="Email" />
+      <Input placeholder="Password" />
     </DefaultView>
 
-    <DefaultView>
-      <Button large title="Create an account" onPress={() => alert('Account created')} />
-      <DefaultView style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
-        <Text>Already have an account?</Text>
-        <Link title="Sign in" onPress={() => navigation.navigate(AuthScreenName.SIGN_IN)} />
-      </DefaultView>
+    <Button large title="Create an account" onPress={() => alert('Account created')} />
+    <DefaultView style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+      <Text>Already have an account?</Text>
+      <Link title="Sign in" onPress={() => navigation.navigate(AuthScreenName.SIGN_IN)} />
     </DefaultView>
   </Layout>
 )
