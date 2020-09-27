98613bf4aab3071d8da095754c8ef51f9964dc8b
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-09-27 12:11:39 +0100

    add image grid component

---
+++ app/components/organisms/ImageGrid/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import { Dimensions, FlatList } from 'react-native'
+
+type Props = {
+  images: any[]
+}
+
+const { width } = Dimensions.get('window');
+
+const ImageContainer = styled.TouchableOpacity`
+  width: ${ props => (width-20)/4 }px; /** minus 20px layout padding from width */
+  height: ${ props => (width-20)/4 }px; /** minus 20px layout padding from width */
+  margin-bottom: 4px;
+`
+
+const Image = styled.Image`
+  flex: 1;
+  border-radius: 4px;
+`
+
+const ImageGrid = ({ images }: Props) => {
+  const renderItem = ({ item, index }: any) => {
+    return (
+      <ImageContainer
+        onPress={() => alert(`Image: ${item._id} clicked`)}
+        style={[index % 4 !==0 ? {paddingLeft: 4} : {paddingLeft: 0}]}>
+        <Image source={{ uri: item.image }} />
+      </ImageContainer>
+    )
+  }
+
+  return (
+    <>  
+      <FlatList
+        horizontal={false}
+        scrollEnabled={ images.length > 2 ? true : false }
+        data={images}
+        renderItem={renderItem}
+        keyExtractor={(item: any) => item._id}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        numColumns={4}
+      />
+    </>
+  )
+}
+
+
+
+export { ImageGrid }
--- app/screens/Root/Profile.tsx
+++ app/screens/Root/Profile.tsx
@@ -4,32 +4,63 @@ import { Dimensions, ImageBackground, View as DefaultView, Image } from 'react-n
 import { AntDesign } from '@expo/vector-icons'
 import { RootScreenName, RootStackParamList } from '../../navigation/types'
 import { theme } from '../../components/Theme'
-import { Layout, Text, Input, Thumbnail, ProfileHeader, ProfileSwitch } from '../../components'
+import { Layout, Text, Input, Thumbnail, ProfileHeader, ProfileSwitch, ImageGrid } from '../../components'
 import { Row } from 'native-base'
 
 const images = [
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4",
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4",
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4",
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4",
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4",
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4",
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4",
-  "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  {
+    _id: '001',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '002',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '003',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '004',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '005',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '006',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '007',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '008',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
+  {
+    _id: '008',
+    image: "https://dummyimage.com/150x150/c4c4c4/c4c4c4"
+  },
 ]
 
 const { width } = Dimensions.get('window')
 const Profile = ({ navigation }: StackScreenProps<RootStackParamList, RootScreenName.PROFILE>) => {
   return (
+    <>
     <Layout>
       <ProfileHeader
-        avatar={images[0]}
+        avatar={images[0].image}
         name="Jane Brown"
         username="@Janebrown20"
         bio= "Atque ratione saepe reiciendis omnis omnis esse. Aut quia porro quam et. Qui odio veritatis et adipisci."
         />
       <ProfileSwitch />
+      <ImageGrid images={images} />
     </Layout>
+    </>
   )
 }
 
