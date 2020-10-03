a92c6474ca106c67b7a8463e17e07675400e1eb0
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-10-03 20:09:10 +0100

    add ingress config

--- services/authentication/src/app.ts
+++ services/authentication/src/app.ts
@@ -11,7 +11,7 @@ export default async (): Promise<express.Application> => (
       app.use(express.urlencoded({ extended: false }))
 
       // test api route
-      app.get('/api/user/', async (req: express.Request, res: express.Response): Promise<express.Response> => {
+      app.get('/api/users/', async (req: express.Request, res: express.Response): Promise<express.Response> => {
         return res.json({ 'Mainstreet Authentication API': 'Version 1' })
       })
 
