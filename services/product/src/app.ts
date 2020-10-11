c47d089c168fcf9a2db42a94630162db6ec53a25
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-10-11 20:22:07 +0100

    add product service skaffold

---
+++ services/product/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import { RegistrableController } from './api/registrable.controller'
+import container from './inversify.config'
+import TYPES from './types'
+import logger from './utilities/logger'
+
+export default async (): Promise<express.Application> => (
+  new Promise<express.Application>(async (resolve, reject) => {
+    try {
+      const app = express()
+
+      // set middleware
+      app.use(express.json())
+      app.use(express.urlencoded({ extended: false }))
+
+      // register api routes
+      const controllers: RegistrableController[] = container.getAll<RegistrableController>(TYPES.Controller)
+      controllers.forEach(controller => controller.registerRoutes(app))
+      
+      // test api route
+      app.get('/api/user/', async (req: express.Request, res: express.Response): Promise<express.Response> => {
+        return res.json({ 'Mainstreet Authentication API': 'Version 1' })
+      })
+
+      resolve(app)
+      
+    } catch (error) {
+      logger.error(`Error when bootstrapping app: ${error}`)
+      reject(error)
+    }
+  })
+)
---
+++ services/product/src/index.ts
@@ -0,0 +1,23 @@
+import bootstrapApp from './app'
+// import connectToDbClient from './database/db_client'
+import logger from './utilities/logger'
+
+const runApp = async () => {
+  try {
+    const PORT = process.env.PORT || 3000
+
+    // connect to database
+    // await connectToDbClient()
+
+    const app = await bootstrapApp()
+  
+    app.listen(PORT, () => logger.debug(`App running on PORT: ${PORT}`))
+  
+    return app
+  
+  } catch(error) {
+    logger.error(`Unable to run app: ${error}`)
+  }
+}
+
+( async () => await runApp() )()
