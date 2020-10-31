83ef6b632ba1acba28b2ba4ba4dae88d0ac00fcb
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-10-31 22:35:53 +0000

    add order status enum

--- services/product/src/packages/mainstreet-nats/domain/enums.ts
+++ services/product/src/packages/mainstreet-nats/domain/enums.ts
@@ -3,3 +3,18 @@ export enum NatsEventSubjectEnum {
   PRODUCT_UPDATED = 'product:updated'
 }
 
+export enum OrderStatusEnum {
+  // when order has been created but product has not been reserved
+  CREATED = 'created',
+
+  // when the product the order is trying to reserve has been reserved already
+  // when the user cancels the order
+  CANCELLED = 'cancelled',
+
+  // the order has successfully reserved the product
+  AWAITING_PAYMENT = 'awaiting:payment',
+
+  // the order has successfully reserved the product
+  // the product has been paid for
+  COMPLETED = 'completed',
+}
