7327b50837dcd42fb74f56bde0032e0ba1ceadec
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-10-31 17:43:12 +0000

    add NATS env variables

--- services/product/src/index.ts
+++ services/product/src/index.ts
@@ -11,7 +11,11 @@ const runApp = async () => {
     await connectToDbClient()
 
     // connect to NATS
-    await natsClient.connect("mainstreet", "abcd", "http://nats-service:4222")
+    if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined')
+    if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID must be defined')
+    if (!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID must be defined')
+    await natsClient.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
+
     // handle NATS termination
     natsClient.client.on('close', () => {
       logger.debug(`Connection to NATS terminated`)
