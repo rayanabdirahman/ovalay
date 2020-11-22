d5c62ed11f7869bcf0d63b72ef5700d416c674ee
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-11-22 20:44:29 +0000

    add caption component

---
+++ app/components/atoms/Caption/index.tsx
@@ -0,0 +1,16 @@
+import { TextInput as DefaultTextInput } from 'react-native'
+import styled from 'styled-components/native'
+import * as React from 'react'
+
+type CaptionProps = DefaultTextInput['props'] & {}
+
+const Caption = styled.TextInput<CaptionProps>`
+  padding: 20px 0 20px 24px;
+  background-color: ${({ theme }) => theme.colour.white};
+  border-width: 1px;
+  border-color: ${({ theme }) => theme.colour.grey};
+  margin-bottom: 16px;
+  max-height: 100px;
+`
+
+export default Caption
