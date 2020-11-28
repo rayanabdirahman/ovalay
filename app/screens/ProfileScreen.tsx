4993ae44f6a4fbfe69b86eb49ece5d8caac5161d
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-11-28 22:43:50 +0000

    show spinner if products is null

--- app/screens/ProfileScreen.tsx
+++ app/screens/ProfileScreen.tsx
@@ -1,30 +1,32 @@
 import { useNavigation } from '@react-navigation/native'
-import { Button, Text, View } from 'react-native'
+import { ActivityIndicator, Button, Text, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import * as React from 'react'
 
 import Layout from '../components/Layouts'
 import { getProducts } from '../store/actions/product'
 import { State } from '../store'
+import { ProductState } from '../store/types'
 import { ProfileTabRouteName } from '../navigation/types'
+import { theme } from '../components/Theme'
 
 export default function ProfileScreen() {
   const navigation = useNavigation()
   const dispatch = useDispatch()
-  const { product } = useSelector<State, State>(state => state)
+  const { products } = useSelector<State, ProductState>(state => state.product)
 
   React.useEffect(() => {
     navigation.setOptions({})
-
     // dispatch action to get products
     dispatch(getProducts())
   }, [navigation])
 
-  return (
+  return !products ?
+    <ActivityIndicator size="small" color={theme.colour.black} /> : (
     <Layout>
-      <Text>Profile Screen- product num: {product.products.length}</Text>
+      <Text>Profile Screen- product num: {products.length}</Text>
       {
-        product.products.map((product, index) => (
+        products.map((product, index) => (
           <View key={`product--${index}`} style={{ marginBottom: 20 }}>
             <Button
               onPress={() => navigation.navigate(ProfileTabRouteName.PRODUCT_SCREEN, { productId: product._id })}
